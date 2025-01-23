a1e23c31aa4854671b859cd50ee4da6a747e68e6
Augusto Lima <losgutbov@gmail.com>
2025-01-23 18:51:33 -0300

    Breaking passwords with brutal force

---
+++ Quebrando senhas básicas com força bruta/quebrando_senha_forca_bruta.py
@@ -0,0 +1,15 @@
+import hashlib
+
+
+hash_original = ""
+with open("senhas.txt", "r") as file:
+    hash_original = file.readline()
+
+
+
+senha = "password"
+hash_md5 = hashlib.md5(senha.encode()).hexdigest()
+if hash_md5 == hash_original:
+    print(f"A senha {senha} é a senha correta")
+    print(hash_md5)
+
